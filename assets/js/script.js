66cc763069eb01125e04cde729f201b66679e71b
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-12 17:58:20 -0500

    fetch longitude and lattitude of a city functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log('connected')
+var APIkey = '05818f2d35db6557de278dbcb6229116';
+var city = prompt();
+var latitude;
+var longitude;
+var url = 'http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid='+APIkey;
+//API call to retrieve longitude and latitude of a city
+
+var coordinatesUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+APIkey
+fetch(coordinatesUrl) //fetches data using city name and we can get the longitude and lattitude from that
+.then(function(res){
+    return res.json()
+}).then(function(data){
+    console.log('longitude',data[0].lon)
+    console.log('lattitude',data[0].lat)
+    latitude = data[0].lon;
+    longitude = data[0].lat;
+})
+//API calling with city name

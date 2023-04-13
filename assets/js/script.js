a240dd132a7afe4ac71b31a0733441980a63c5de
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-12 22:52:30 -0500

    added weather api calling functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,50 @@
 var APIkey = '05818f2d35db6557de278dbcb6229116';
-var city = prompt();
-var latitude;
-var longitude;
-var url = 'http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid='+APIkey;
-//API call to retrieve longitude and latitude of a city
+
+//selectors
+var searchBtn = $('#search-button');
+var inputEl = $('#input-city');
+var cityDisplay = $('#city-display');
+var dateDisplay = $('#date-display')
+var tempDisplay = $('#temp-display')
+//a function that generates a weather data of a city
+function getWeatherData(city){
+    //API call to retrieve longitude and latitude of a city
 
 var coordinatesUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+APIkey
-fetch(coordinatesUrl) //fetches data using city name and we can get the longitude and lattitude from that
+fetch(coordinatesUrl) //fetches data using city name to get the longitude and lattitude of that city
 .then(function(res){
     return res.json()
 }).then(function(data){
     console.log('longitude',data[0].lon)
     console.log('lattitude',data[0].lat)
-    latitude = data[0].lon;
-    longitude = data[0].lat;
+    var latitude = data[0].lat;
+    var longitude = data[0].lon;
+    
+ //API calling  which uses longitude and lattitude to fetch data
+ 
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid='+APIkey;
+    fetch(queryUrl) //fetch using coordinates
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        
+        console.log(data.city.name)
+        cityDisplay.text(data.city.name);
+        dateDisplay.text(dayjs().format('MM/DD/YYYY'))
+        tempDisplay.text(Math.floor((data.list[0].main.temp-273.15)*9/5)+32+' C')
+
+
+    })
+   
 })
-//API calling with city name
+
+}
+
+
+//event listener
+searchBtn.on('click',function(){
+    var cityName = $('#input-city').val();
+
+    getWeatherData(cityName);
+    
+})
